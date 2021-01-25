6f53ef47fb5a79c08c032732858a1b35e4a5e1bf
vananjeeva <vananjeeva@gmail.com>
2021-01-25 11:57:27 +0530

    25/01 11.57

--- src/main/java/com/inses/api/UserResource.java
+++ src/main/java/com/inses/api/UserResource.java
@@ -3,6 +3,7 @@ package com.inses.api;
 import com.inses.modal.User;
 import com.inses.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/get")
+    @GetMapping("/get")
     private List<User> getAllPost() throws ExecutionException, InterruptedException {
         return userService.getAllUser();
     }
