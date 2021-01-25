7b77de0817688ba1384b7b710fb6a7d76a76ed33
vananjeeva <vananjeeva@gmail.com>
2021-01-25 11:21:27 +0530

    25/01 11.21

---
+++ src/main/java/com/inses/InsesApplication.java
@@ -0,0 +1,12 @@
+package com.inses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsesApplication.class, args);
+    }
+}
