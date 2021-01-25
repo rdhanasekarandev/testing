d47e4693cb3a89f33dd64b376ec191abf1c15391
vananjeeva <vananjeeva@gmail.com>
2021-01-25 16:19:10 +0530

    25/01 16:18

---
+++ src/main/java/com/inses/api/ImageController.java
@@ -0,0 +1,39 @@
+package com.inses.api;
+
+import com.inses.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(value = "image")
+public class ImageController {
+
+    @Autowired
+    ImageService imageService;
+
+    @PostMapping
+    public ResponseEntity create(@RequestParam(name = "file") MultipartFile[] files) {
+
+        for (MultipartFile file : files) {
+
+            try {
+
+                String fileName = imageService.save(file);
+
+                String imageUrl = imageService.getImageUrl(fileName);
+
+                // do whatever you want with that
+
+            } catch (Exception e) {
+                //  throw internal error;
+            }
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/com/inses/service/FirebaseService.java
+++ src/main/java/com/inses/service/FirebaseService.java
@@ -3,6 +3,11 @@ package com.inses.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.inses.api.ImageResource;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +15,8 @@ import java.io.FileInputStream;
 
 @Service
 public class FirebaseService {
+    @Autowired
+    ImageResource.Properties properties;
     @PostConstruct
     private void initialize() {
         try {
@@ -18,6 +25,7 @@ public class FirebaseService {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://inses-73ab3-default-rtdb.firebaseio.com")
+                    .setStorageBucket(properties.getBucketName())
                     .build();
 
             FirebaseApp.initializeApp(options);
@@ -25,4 +33,14 @@ public class FirebaseService {
             e.printStackTrace();
         }
     }
+    @Data
+    @Configuration
+    @ConfigurationProperties(prefix = "firebase")
+    public class Properties {
+
+        private String bucketName;
+
+        private String imageUrl;
+    }
+
 }
