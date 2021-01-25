29a50142d5b1e55f8c8942d6a744273994dd3c5a
vananjeeva <vananjeeva@gmail.com>
2021-01-26 04:59:10 +0530

    26/01 4:58

--- src/main/java/com/inses/api/UserResource.java
+++ src/main/java/com/inses/api/UserResource.java
@@ -15,15 +15,15 @@ public class UserResource {
     private UserService userService;
 
 
-    
+
     //  create new User
     @PostMapping("/save")
     public String saveUserDetails(@RequestBody User user){
         return userService.createUser(user);
     }
 
+    
     //  get all users
-    @CrossOrigin
     @GetMapping("/get")
     private List<User> getAllPost() throws ExecutionException, InterruptedException {
         return userService.getAllUser();
